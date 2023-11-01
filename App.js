6d7f69e5d4ee6fac908940bfe48217aa963f1026
Rambabu-Gudipati <94040452+Rambabu-Gudipati@users.noreply.github.com>
2023-11-01 17:27:35 +0000

    successfully working

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 
-const mainContianer = React.createElement("div", {id: "parent"}, 
-React.createElement("div", {id: "child"}, React.createElement("h1", {id: "reactheading"}, "Rambabu React")))
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(mainContianer)
+
+const mainContianer = React.createElement("div", {id: "parent"}, 
+React.createElement("div", {id: "child"}, 
+React.createElement("h1", {id: "reactheading"}, "Rambabu React")))
+
+const rootElement = ReactDOM.createRoot(document.getElementById("root"))
+rootElement.render(mainContianer)
