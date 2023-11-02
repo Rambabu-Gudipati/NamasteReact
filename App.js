d1ab0eba76ec0216c3b1ce44f6ab6b1f8e0473ea
Rambabu-Gudipati <94040452+Rambabu-Gudipati@users.noreply.github.com>
2023-11-02 17:26:49 +0000

    react child elements added

--- App.js
+++ App.js
@@ -2,8 +2,13 @@
 
 
 const mainContianer = React.createElement("div", {id: "parent"}, 
-React.createElement("div", {id: "child"}, 
-React.createElement("h1", {id: "reactheading"}, "Rambabu React")))
+[React.createElement("div", {id: "child1"}, 
+[React.createElement("h1", {id: "reactheading"}, "This is h1 tag"),
+React.createElement("h1", {id: "reactheading"}, "This is h2 tag"), ]),
+React.createElement("div", {id: "child2"}, 
+[React.createElement("h1", {id: "reactheading"}, "This is h1 tag"),
+React.createElement("h1", {id: "reactheading"}, "This is h2 tag") ])]
+)
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"))
 rootElement.render(mainContianer)
