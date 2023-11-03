fddf8ad04c0d7e2497ae4f239acd18d08f191c78
Rambabu-Gudipati <94040452+Rambabu-Gudipati@users.noreply.github.com>
2023-11-03 18:20:47 +0000

    react one child element removed

--- App.js
+++ App.js
@@ -6,8 +6,7 @@ const mainContianer = React.createElement("div", {id: "parent"},
 [React.createElement("h1", {id: "reactheading"}, "This is h1 tag"),
 React.createElement("h1", {id: "reactheading"}, "This is h2 tag"), ]),
 React.createElement("div", {id: "child2"}, 
-[React.createElement("h1", {id: "reactheading"}, "This is h1 tag"),
-React.createElement("h1", {id: "reactheading"}, "This is h2 tag") ])]
+[React.createElement("h1", {id: "reactheading"}, "This is h1 tag") ])]
 )
 
 const rootElement = ReactDOM.createRoot(document.getElementById("root"))
