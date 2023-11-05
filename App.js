0e4013e50c7f6d69d064554f62562429aa920551
Rambabu-Gudipati <94040452+Rambabu-Gudipati@users.noreply.github.com>
2023-11-05 17:46:21 +0000

    main element removed

--- App.js
+++ App.js
@@ -3,8 +3,7 @@
 
 const mainContianer = React.createElement("div", {id: "parent"}, 
 [React.createElement("div", {id: "child1"}, 
-[React.createElement("h1", {id: "reactheading"}, "This is h1 tag"),
-React.createElement("h1", {id: "reactheading"}, "This is h2 tag"), ]),
+),
 React.createElement("div", {id: "child2"}, 
 [React.createElement("h1", {id: "reactheading"}, "This is h1 tag"),
 React.createElement("h1", {id: "reactheading"}, "This is h2 tag") ])]
