e3c3beb155065a55332ba3cb9e3265c226367ce2
Rambabu-Gudipati <94040452+Rambabu-Gudipati@users.noreply.github.com>
2023-11-05 17:46:42 +0000

    element added

--- App.js
+++ App.js
@@ -3,7 +3,8 @@
 
 const mainContianer = React.createElement("div", {id: "parent"}, 
 [React.createElement("div", {id: "child1"}, 
-),
+[React.createElement("h1", {id: "reactheading"}, "This is h1 tag"),
+React.createElement("h1", {id: "reactheading"}, "This is h2 tag"), ]),
 React.createElement("div", {id: "child2"}, 
 [React.createElement("h1", {id: "reactheading"}, "This is h1 tag"),
 React.createElement("h1", {id: "reactheading"}, "This is h2 tag") ])]
